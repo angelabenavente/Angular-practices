2dedfaf0fe757497f081447d6effe65bda0b4e39
angelabenavente <angela.b.almazan@gmail.com>
2020-03-31 14:08:41 +0200

    Use of modules

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `
+  <p>This is a warnign alert. You're in danger!</p>
+  `
+})
+export class WarningAlertComponent {
+
+}
