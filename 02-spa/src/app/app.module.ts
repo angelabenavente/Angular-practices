84e199c9d47f7e4d06d971d1cc581aa3ddf85af8
angelabenavente <angela.b.almazan@gmail.com>
2020-05-08 12:18:39 +0200

    Add navbar

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { HerosComponent } from './components/heros/heros.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
+    HerosComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
---
+++ 02-spa/src/app/components/heros/heros.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html'
+})
+export class HerosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
