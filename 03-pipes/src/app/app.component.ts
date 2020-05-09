4915c93fe10c315a30839cf82c2dba9c532f7e9b
angelabenavente <angela.b.almazan@gmail.com>
2020-05-09 22:03:14 +0200

    Add slice examples

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   nombre: string = 'Capitán América';
+  nombre: string = 'Capitán América';
+  array: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
