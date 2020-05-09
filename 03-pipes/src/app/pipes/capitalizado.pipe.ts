5149ffea97d2df09e370e4695101287d4951efde
angelabenavente <angela.b.almazan@gmail.com>
2020-05-09 23:46:41 +0200

    Fix error personalize pipe

--- 03-pipes/src/app/pipes/capitalizado.pipe.ts
+++ 03-pipes/src/app/pipes/capitalizado.pipe.ts
@@ -17,7 +17,6 @@ export class CapitalizadoPipe implements PipeTransform {
       
     } else {
       nombres[0] = nombres[0][0].toUpperCase() + nombres[0].substring(1);
-      return 'hola mundo';
     }
     return nombres.join(' ');
   }
